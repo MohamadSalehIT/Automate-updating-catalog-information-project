b4470e88a149e0553b5b2e29bb89c7d60793bf48
Mohamad Saleh <123941794+MohamadSalehIT@users.noreply.github.com>
2023-03-25 17:58:33 +0200

    Update changeImage.py

--- changeImage.py
+++ changeImage.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 from PIL import Image
 import os
-
+#This is the path where the images and txt files are located
 path = "./supplier-data/images/"
-for f in os.listdir("./supplier-data/images"):
+
+for f in os.listdir(path):
+    #checking if the image ends with .tiff , we will replace it with .jepg
     if f.endswith(".tiff"):
         split_f = f.split(".")
         name = split_f[0] + ".jpeg"
         im = Image.open(path + f).convert("RGB")
-        im.resize((600, 400)).save("./supplier-data/images/" + name, "JPEG")
+        #resizing the images and saving them in the same directory
+        im.resize((600, 400)).save(path + name, "JPEG")
 
 
